cefbd869c6a4725626bf7a0835ce29cfb4cf4a9b
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-10-24 15:16:33 -0300

    Atualização da pasta front-web

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">Vendas Dash</h1>
+    </header>
+  );
+}
+
+export default Header;
